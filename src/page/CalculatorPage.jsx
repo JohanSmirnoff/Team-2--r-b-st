0d228f62ea884ce35643f4aaa675f25c1fce5fa6
JohanSmirnoff <smirnoff.johan@gmail.com>
2025-12-04 11:27:44 +0100

    kalkylator motherfuckers

---
+++ src/page/CalculatorPage.jsx
@@ -0,0 +1,11 @@
+import Calculator from "../component/calculator/Calculator";
+
+const CalculatorPage = () => {
+    return(
+        <div>
+            <Calculator />
+        </div>
+    )
+}
+
+export default CalculatorPage
